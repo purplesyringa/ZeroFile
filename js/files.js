577eaa709a086426a8e2c8b8ba527433e5100cf6
Ivanq <imachug@yandex.ru>
2017-05-17 20:15:48 +0400

    Files: Show

--- js/files.js
+++ js/files.js
@@ -4,6 +4,90 @@ class Files {
 		this.shownPathDelim = " " + String.fromCharCode(8250) + " "; /* right arrow */
 		this.root = "files";
 		this.showRoot = "Root";
+
+		this.types = {
+			image: [
+				"jpg", // JPEG
+				"jpeg",
+				"jp2", // JPEG 200
+				"jpx",
+				"tiff", // Tiff
+				"gif", // GIF
+				"bmp", // BMP
+				"png", // PNG
+				"webp", // WebP
+				"bpg", // Better Portable Graphics
+				"svg" // SVG
+			],
+			music: [
+				"3gp", // 3GP
+				"aiff", // AIFF
+				"au", // Au
+				"flac", // Flac
+				"gsm", // GSM
+				"m4a", // M4A
+				"mp3", // MP3
+				"ogg", // Ogg
+				"oga",
+				"mogg",
+				"vox", // vox
+				"wav", // WAV
+				"wma" // WMA
+			],
+			video: [
+				"mkv", // MKV
+				"ogv", // Ogg
+				"avi", // AVI
+				"mov", // MOV
+				"wmv", // WMV
+				"mp4", // MP4
+				"mpg", // MPEG
+				"mpeg",
+				"m4v" // M4V
+			],
+			text: [
+				"doc", // Doc
+				"docx",
+				"latex", // LaTeX
+				"tex",
+				"text", // TXT
+				"txt"
+			]
+		};
+	}
+
+	guessType(file) {
+		return this.fs.getType(file)
+			.then(type => {
+				if(type == "dir") {
+					return "dir";
+				}
+
+				let filename = file.split("/").slice(-1)[0].split(".");
+
+				let ext;
+				if(filename.length == 1) {
+					ext = "";
+				} else {
+					ext = filename[filename.length - 1].toLowerCase();
+				}
+
+				return Object.keys(this.types).find(type => this.types[type].indexOf(ext) > -1) || "file";
+			});
+	}
+	getFiles(path) {
+		return this.fs.readDirectory(path)
+			.then(files => {
+				return ZeroPage.async.map(files, file => {
+					return this.guessType((path ? path + "/" : "") + file)
+						.then(type => {
+							return {
+								type: type,
+								name: file
+							};
+						});
+				});
+			});
 	}
 
 	load(path) {
@@ -11,22 +95,31 @@ class Files {
 			(this.showRoot ? [this.showRoot] : [])
 				.concat(path ? path.split("/") : []).join(this.shownPathDelim)
 		);
+
+		return this.getFiles(path)
+			.then(files => {
+				let filesNode = document.getElementById("files");
+
+				files.forEach(file => {
+					let fileNode = document.createElement("div");
+					fileNode.className = "file" + (file.type == "dir" ? " file-dir" : "");
+
+					let fileIconContainer = document.createElement("div");
+					fileIconContainer.className = "file-icon-container";
+					let fileIcon = document.createElement("img");
+					fileIcon.className = "file-icon";
+					fileIcon.src = "img/file/" + file.type + ".png";
+					fileIconContainer.appendChild(fileIcon);
+					fileNode.appendChild(fileIconContainer);
+
+					let link = document.createElement("a");
+					link.className = "file-name";
+					link.href = "?" + (path ? path + "/" : "") + file.name;
+					link.textContent = file.name;
+					fileNode.appendChild(link);
+
+					filesNode.appendChild(fileNode);
+				});
+			});
 	}
 };
-
-/*
-<div class="file">
-	<div class="file-icon-container">
-		<img class="file-icon" src="img/file/text.png">
-	</div>
-
-	<a class="file-name" href="fsdgfdg">fsdgfdg</a>
-</div>
-<div class="file">
-	<div class="file-icon-container">
-		<img class="file-icon" src="img/file/image.png">
-	</div>
-
-	<a class="file-name" href="fsdgfdg">fsdgfdg</a>
-</div>
-*/
